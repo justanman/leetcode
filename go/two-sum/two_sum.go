1210c20f24192e45bbfc4750733a1043b17d1a91
Justin Tan <justanman@users.noreply.github.com>
2017-10-21 17:08:50 -0400

    add 'two sum' solution

--- go/two-sum/two_sum.go
+++ go/two-sum/two_sum.go
@@ -12,6 +12,26 @@ func twoSum(nums []int, target int) []int {
 	}
 	return indices
 }
-
 // time O(n^2)
+// space O(1)
+
+func twoSumMap(nums []int, target int) []int {
+  table := make(map[int]int)
+  indices := make([]int, 2)
+  for i := 0; i < len(nums); i++ {
+    table[nums[i]] = i
+  }
+  for i := 0; i < len(nums); i++ {
+    complement := target - nums[i]
+    if _, ok := table[complement]; ok {
+      if table[complement] != i {
+        indices[0] = i
+        indices[1] = table[complement]
+        return indices
+      }
+    }
+  }
+  return indices
+}
+// time O(n)
 // space O(1)
